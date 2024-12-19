e871af17f62c0da03d1923e10ac37b684efe8e4e
David_Schneider <davesh0812@gmail.com>
2024-12-19 13:52:11 +0200

    fix test

--- mlrun/serving/states.py
+++ mlrun/serving/states.py
@@ -777,7 +777,7 @@ class RouterStep(TaskStep):
                 handler=handler,
                 model_endpoint_creation_strategy=creation_strategy,
                 endpoint_type=schemas.EndpointType.LEAF_EP
-                if "serving.VotingEnsemble" in self.class_name
+                if self.class_name and "serving.VotingEnsemble" in self.class_name
                 else schemas.EndpointType.NODE_EP,
             )
         route.function = function or route.function
